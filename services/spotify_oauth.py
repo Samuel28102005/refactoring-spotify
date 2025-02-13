12c7d21d0a5651644c928bf7d3159fadec3a2c65
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2025-02-13 21:39:38 +0000

    finito..err

--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -8,6 +8,9 @@ SPOTIFY_CLIENT_SECRET = "1355c56f1fa24cc0964580c2df0d3839"
 SPOTIFY_REDIRECT_URI = "https://5000-samuel281020-spotifyapi-si6z4qrwax9.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 #config SpotifyOAuth per l'autenticazione e redirect uri
 #config SpotifyOAuth per l'autenticazione e redirect uri
+
+
+#config SpotifyOAuth per l'autenticazione e redirect uri
 sp_oauth = SpotifyOAuth(
     client_id=SPOTIFY_CLIENT_ID,
     client_secret=SPOTIFY_CLIENT_SECRET,
